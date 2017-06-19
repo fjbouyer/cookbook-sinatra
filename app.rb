a62d1ebb99580a6b758d9586503f8ade8052a2f8
Francois Joseph Bouyer <fjbouyer@gmail.com>
2017-06-19 16:41:06 +0200

    dddd

--- app.rb
+++ app.rb
@@ -6,7 +6,9 @@ configure :development do
   use BetterErrors::Middleware
   BetterErrors.application_root = File.expand_path('..', __FILE__)
 end
+set :bind, '0.0.0.0'
 
-get '/' do
-  'Hello world!'
+get '/layout' do
+  erb :about
 end
+
